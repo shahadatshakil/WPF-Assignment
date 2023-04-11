eaa9ccf30729e621611efcd7448f026e36473c92
ENOSISBD\shakil <shahadat.shakil@enosisbd.com>
2023-04-11 19:23:51 +0600

    Minor changes

--- NewYorkTimesBestSellers/Constants.cs
+++ NewYorkTimesBestSellers/Constants.cs
@@ -3,6 +3,5 @@
     public static class Constants
     {
         public const string BookRetrieveAPI = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=Wuc1leNpfOz8EoAsolaJFOMpCAtzxkM9";
-
     }
 }
